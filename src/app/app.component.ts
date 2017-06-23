5bd980a1c759b9145f686c8a53b426b1299bc794
Colin Mackey <colin.mackey92@gmail.com>
2017-06-23 04:16:50 +0100

    Protoype idea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Shit r/Streetfighter Says';
+  quote= 'I hearby challenge you to a 1v1 (no chun li) ill happily kick ur ass anytime. I lagged at the end of that game I admit it. Is it a crime to dc cause my internet goes out? I don\'t think so. Bring it bitch';
 }
